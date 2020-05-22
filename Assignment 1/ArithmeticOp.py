97c8f7bb4ff84353362305370e9088a0330b2182
Devanshi2660 <65752834+Devanshi2660@users.noreply.github.com>
2020-05-22 19:35:26 +0530

    Add files via upload

---
+++ Assignment 1/ArithmeticOp.py
@@ -0,0 +1,12 @@
+a = input('Enter num1:')
+b = input('Enter num2:')
+
+add = float(a) + float(b)
+sub = float(a) - float(b)
+mul = float(a) * float(b)
+div = float(a) / float(b)
+
+print('Sum:',add)
+print('Difference:',sub)
+print('Product:', mul)
+print('Quotient:', div) 
